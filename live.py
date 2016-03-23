304be86677a9ed9efbaa8483b307099b491d593b
ian <ianfan98@gmail.com>
2016-03-23 00:38:03 -0700

    moved parse segmentation

--- live.py
+++ live.py
@@ -31,7 +31,7 @@ def liveEdge(startNumber, timeOffset, segDuration, sugPreDelay):
   edge = startNumber + (timeOffset-sugPreDelay)/segDuration
   #print('edge =	', edge, hex(math.floor(edge)))
   #print('Live Edge Segment Index = ', hex(math.floor(edge))[2:])
-  print( '%*s = 0x%*s' % (-30, 'LIVE Edge Segment Index', -30, hex(math.floor(edge))[2:]))
+  print( '  %*s = 0x%*s' % (-30, 'LIVE Edge Segment Index', -30, hex(math.floor(edge))[2:]))
   return
 
 def epoTime(isoTime):
@@ -41,6 +41,60 @@ def epoTime(isoTime):
  # newt = (tmp[0], tmp[1], tmp[3], tmp[4], tmp[5], tmp[6], tmp[7],tmp[8],0)
  # epo = time.mktime(newt)
   return tmp
+
+
+def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd):
+  
+  # attributes from Period SegmentTemplate
+
+  # use full name
+  ns = mpd_xmlns + 'SegmentTemplate'
+  for st in root.iter(ns):
+    ts = 1.0
+    if 'timescale' in st.attrib:
+      ts = float(st.attrib['timescale'])
+    else:
+      ts = 1.0
+    if 'duration' in st.attrib:
+      st_time = float(st.attrib['duration'])/ts
+    else:
+      st_time = 1
+
+    if 'presentationTimeOffset' in st.attrib:
+      print( '  %*s = %*s' % (-30, 'presentationTimeOffset', -30, st.attrib['presentationTimeOffset']))
+     
+    sn = int(st.attrib['startNumber'])
+    print( '  %*s = %*s' % (-30, 'Segment Duration', -30, st_time))
+    print( '  %*s = %*s' % (-30, 'StartNumber', -30, sn))
+
+    t2 = datetime.utcnow()
+
+    print( '  %*s = %*s' % (-30, 'Time NOW from Client', -30, t2.isoformat()))
+    print( '  %*s = %*s' % (-30, 'Time NOW from Server', -30, h_datetime.isoformat()))
+    print( '  %*s = %*s' % (-30, 'availabilityStartTime', -30, mpd_ast))
+
+    #t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%SZ') # not all has Z in it
+    mpd_ast = mpd_ast.strip('Z') 
+    t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%S')
+    d = t2 - t1
+    timeOffset = d.total_seconds()
+    print( '  %*s = %*s' % (-30, 'timeOffset', -30, timeOffset))
+
+    if mpd_aet != 'NA':
+      mpd_aet = mpd_aet.strip('Z') 
+      t3 = datetime.strptime(mpd_aet, '%Y-%m-%dT%H:%M:%S')
+      d = t3 - t1
+     # print('availability window size=', d.total_seconds(), ' sec ')
+      print( '  %*s = %*s seconds' % (-30, 'availability window', -10, d.total_seconds()))
+    
+    if timeOffset > 0 :
+      liveEdge(sn, timeOffset, st_time, 22.5)
+    else:
+      print( '  %*s = %*s' % (-30, 'Content Available in', -10, -timeOffset))
+      
+  return
+
+
 
 # return second from PT time
 # input can be PT30S, PT4M, PT4M30S, PT2.04800000S, PT20082.815918S
@@ -78,7 +132,7 @@ mpd = f.read()
 root = ET.fromstring(mpd.decode('utf-8'))
 
 # show MPD
-#print(mpd.decode('utf-8'))
+print(mpd.decode('utf-8'))
 
 # get MPD level attributes
 
@@ -132,7 +186,7 @@ print( '%*s = %*s' % (-30, 'suggestedPresentationDelay', -30, mpd_spd))
 print( '%*s = %*s' % (-30, 'minBufferTime', -30, mpd_mbt))
 print( '%*s = %*s' % (-30, 'mediaPresentationDuration', -30, mpd_mpd))
 
-# Show Period attributes
+# Show Period attributes: id, duration, start
 nodePeriod = mpd_xmlns + 'Period'
 count=0
 for p in root.findall(nodePeriod):
@@ -156,79 +210,5 @@ for p in root.findall(nodePeriod):
   print( '  %*s = %*s' % (-30, 'id', -30, period_id))
   print( '  %*s = %*s' % (-30, 'duration', -30, period_duration))
   print( '  %*s = %*s' % (-30, 'start', -30, period_start))
-
-
-# check if it is LIVE or VOD
-if mpd_ast == 'NA':
-  print("***  NOT LIVE Service  ***")
-  exit()
-
-# convert iso time to unix epo
-#mpd_pt_epo = time.mktime(time.strptime(mpd_pt, '%Y-%m-%dT%H:%M:%SZ'))
-#mpd_ast_epo = time.mktime(time.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%SZ'))
-if mpd_ast != 'NA':
-  mpd_ast_epo = epoTime(mpd_ast)
-
-#print('pt_epo=', mpd_pt_epo)
-#print('ast_epo=', mpd_ast_epo)
-
-# get relative time
-mpd_mup_f = getSec(mpd_mup)
-mpd_tsbd_f = getSec(mpd_tsbd)
-if(mpd_spd != 'NA'):
-  mpd_spd_f = getSec(mpd_spd)
-else:
-  mpd_spd_f = 0;
-mpd_mbt_f = getSec(mpd_mbt)
-
-#print(mpd_mup_f,mpd_tsbd_f,mpd_spd_f,mpd_mbt_f) 
-
-
-# attributes from Period SegmentTemplate
-
-# sling ns
-ns = mpd_xmlns + 'SegmentTemplate'
-#print('ns=', ns)
-
-# wowza ns
-#ns = '{urn:mpeg:dash:schema:mpd:2011}SegmentTemplate'
-for st in root.iter(ns):
-  ts = 1.0
-  if 'timescale' in st.attrib:
-    ts = float(st.attrib['timescale'])
-  else:
-    ts = 1.0
-  if 'duration' in st.attrib:
-    st_time = float(st.attrib['duration'])/ts
-  else:
-    st_time = 1
-
-  if 'presentationTimeOffset' in st.attrib:
-    print( '%*s = %*s' % (-30, 'presentationTimeOffset', -30, st.attrib['presentationTimeOffset']))
-   
-  sn = int(st.attrib['startNumber'])
-  print( '%*s = %*s' % (-30, 'Segment Duration', -30, st_time))
-  print( '%*s = %*s' % (-30, 'StartNumber', -30, sn))
-
-  t2 = datetime.utcnow()
-
-  print( '%*s = %*s' % (-30, 'Time NOW from Client', -30, t2.isoformat()))
-  print( '%*s = %*s' % (-30, 'Time NOW from Server', -30, h_datetime.isoformat()))
-  print( '%*s = %*s' % (-30, 'availabilityStartTime', -30, mpd_ast))
-
-  #t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%SZ') # not all has Z in it
-  mpd_ast = mpd_ast.strip('Z') 
-  t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%S')
-  d = t2 - t1
-  timeOffset = d.total_seconds()
-  print( '%*s = %*s' % (-30, 'timeOffset', -30, timeOffset))
-
-  if mpd_aet != 'NA':
-    mpd_aet = mpd_aet.strip('Z') 
-    t3 = datetime.strptime(mpd_aet, '%Y-%m-%dT%H:%M:%S')
-    d = t3 - t1
-   # print('availability window size=', d.total_seconds(), ' sec ')
-    print( '%*s = %*s seconds' % (-30, 'availability window', -10, d.total_seconds()))
-
-
-  liveEdge(sn, timeOffset, st_time,mpd_spd_f)
+  doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd)
+  
