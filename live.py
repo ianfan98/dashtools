f1bec8c2ad42243379d180e22593be00de34fc78
ian <ianfan98@gmail.com>
2016-03-23 23:35:07 -0700

    added file input

--- live.py
+++ live.py
@@ -9,24 +9,14 @@ from datetime import datetime
 import time
 import math
 import urllib.request
-
-#sys.stdout.write("Python %s\n" %(sys.version,))
-#print('Num of Args=', len(sys.argv), 'argument')
-#print('Arg list:', str(sys.argv))
-if (len(sys.argv) != 2):
-  print('Usage: \nlive.py mpd_url\n For example, live.py http://vm2.dashif.org/livesim/mup_30/testpic_2s/Manifest.mpd')
-  exit()
-
-if (len(sys.argv) == 2):
-  print('arg=', sys.argv[1])
-  if sys.argv[1].startswith('http'):
-    SrcIsURL = True
-  else:
-    SrcIsURL = False
 #
 # Live Edge
 #
 def liveEdge(startNumber, timeOffset, segDuration, sugPreDelay):
+  print( '  %*s = %*s sec' % (-30, 'timeOffset', -30, timeOffset))
+  print( '  %*s = %*s' % (-30, 'startNumber', -30, startNumber))
+  print( '  %*s = %*s sec' % (-30, 'sugPreDelay', -30, sugPreDelay))
+
   edge = 0;
   edge = startNumber + (timeOffset-sugPreDelay)/segDuration
   #print('edge =	', edge, hex(math.floor(edge)))
@@ -42,8 +32,44 @@ def epoTime(isoTime):
  # epo = time.mktime(newt)
   return tmp
 
+#PT time format 'PTxxxxxHxxxxMxxxxS'
+# return second from PT time
+# input can be PT30S, PT4M, PT4M30S, PT2.04800000S, PT20082.815918S
+#
+def ptTime2Seconds(ptTime):
+  pt = ptTime
+  # shift to left, ie. remove leading PT
+  pt = pt[2:]
 
-def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd):
+  # find H
+  ret = pt.find('H')
+  if ret != -1:
+    hours = pt[:ret]
+  # shift out hours
+    pt = pt[ret+1:]
+  else:
+    hours = 0
+
+  # find M
+  ret = pt.find('M')
+  if ret != -1:
+    mins = pt[:ret]
+    pt = pt[ret+1:]
+  else:
+    mins = 0
+  # find S
+  ret = pt.find('S')
+  if ret != -1:
+    secs = pt[:ret]
+    pt = pt[ret+1:]
+  else:
+    secs = 0
+
+  totalseconds = 3600*float(hours) + 60*float(mins) + float(secs)
+  return totalseconds
+
+
+def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd, period_duration):
   
   # attributes from Period SegmentTemplate
 
@@ -70,7 +96,8 @@ def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd):
     t2 = datetime.utcnow()
 
     print( '  %*s = %*s' % (-30, 'Time NOW from Client', -30, t2.isoformat()))
-    print( '  %*s = %*s' % (-30, 'Time NOW from Server', -30, h_datetime.isoformat()))
+    if(h_datetime != 'NA'):
+      print( '  %*s = %*s' % (-30, 'Time NOW from Server', -30, h_datetime.isoformat()))
     print( '  %*s = %*s' % (-30, 'availabilityStartTime', -30, mpd_ast))
 
     #t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%SZ') # not all has Z in it
@@ -78,7 +105,7 @@ def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd):
     t1 = datetime.strptime(mpd_ast, '%Y-%m-%dT%H:%M:%S')
     d = t2 - t1
     timeOffset = d.total_seconds()
-    print( '  %*s = %*s' % (-30, 'timeOffset', -30, timeOffset))
+    #print( '  %*s = %*s' % (-30, 'timeOffset', -30, timeOffset))
 
     if mpd_aet != 'NA':
       mpd_aet = mpd_aet.strip('Z') 
@@ -88,20 +115,35 @@ def doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd):
       print( '  %*s = %*s seconds' % (-30, 'availability window', -10, d.total_seconds()))
     
     if timeOffset > 0 :
-      liveEdge(sn, timeOffset, st_time, 22.5)
+      if(mpd_spd != 'NA'):
+        val = ptTime2Seconds(mpd_spd)
+      else:
+        val = 0 
+      liveEdge(sn, timeOffset, st_time, val)
+      # progress
+      if(period_duration != 'NA'):
+        print( '  Playback progress now ', round(100*timeOffset/ptTime2Seconds(period_duration)) , '%')
     else:
-      print( '  %*s = %*s' % (-30, 'Content Available in', -10, -timeOffset))
+      print( '  %*s = %*s seconds' % (-30, 'Content Available in', -10, -timeOffset))
       
   return
 
 
 
-# return second from PT time
-# input can be PT30S, PT4M, PT4M30S, PT2.04800000S, PT20082.815918S
-#
-def getSec(ptTime):
-  val = 2
-  return val 
+#sys.stdout.write("Python %s\n" %(sys.version,))
+#print('Num of Args=', len(sys.argv), 'argument')
+#print('Arg list:', str(sys.argv))
+if (len(sys.argv) != 2):
+  print('Usage: \nlive.py <mpd_url | filename>\n For example, live.py http://vm2.dashif.org/livesim/mup_30/testpic_2s/Manifest.mpd')
+  print('      or live.py manifest.mpd')
+  exit()
+
+if (len(sys.argv) == 2):
+  print('arg=', sys.argv[1])
+  if sys.argv[1].startswith('http'):
+    SrcIsURL = True
+  else:
+    SrcIsURL = False
 
 # time Now
 print('Time Now: ', datetime.now())
@@ -111,32 +153,41 @@ print('GMT Time Now: ', datetime.utcnow())
 if(SrcIsURL):
   mpdurl = sys.argv[1]
   #print('src is URL:', mpdurl)
-else: #default
-  mpdurl='http://dash.edgesuite.net/dash264/TestCases/5a/1/manifest.mpd'
-  #print('src is URL Use default')
+  # read from URL
+  try: f = urllib.request.urlopen(mpdurl)
+  except urllib.error.URLError as e:
+    print('URL open error: reason :' + e.reason)
+    exit()
+  info = f.info()
+  headerDate = info.get_all('date')
+  print('Date from Header: ', headerDate[0])
+  # TODO Date format from HTTP is RFC 1123, e.g. "Wed, 16 Mar 2016 22:20:28 GMT"
+  # Need to construct datetime object
+  h_datetime =  datetime.strptime(headerDate[0], '%a, %d %b %Y %H:%M:%S GMT')
+  print('Date from Header ISO format: ', h_datetime.isoformat())
 
-#tree = ET.parse('wowza.mpd')
-#root = tree.getroot()
+  mpd = f.read()
+  root = ET.fromstring(mpd.decode('utf-8'))
 
-# read from URL
-f = urllib.request.urlopen(mpdurl)
-info = f.info()
-headerDate = info.get_all('date')
-print('Date from Header: ', headerDate[0])
-# TODO Date format from HTTP is RFC 1123, e.g. "Wed, 16 Mar 2016 22:20:28 GMT"
-# Need to construct datetime object
-h_datetime =  datetime.strptime(headerDate[0], '%a, %d %b %Y %H:%M:%S GMT')
-print('Date from Header ISO format: ', h_datetime.isoformat())
+  # show MPD
+  print(mpd.decode('utf-8'))
+else: # local file
+  mpdfile = sys.argv[1]
+  f = open(mpdfile) #print('src is URL Use default')
+  buf = f.read()
+  f.close()
 
-mpd = f.read()
-root = ET.fromstring(mpd.decode('utf-8'))
+  tree = ET.parse(mpdfile)
+  root = tree.getroot()
+  # show MPD
+  #print(mp)
 
-# show MPD
-print(mpd.decode('utf-8'))
+  # show MPD
+  print(buf)
+  h_datetime = 'NA'
 
 # get MPD level attributes
-
-mpd_type=root.attrib['type']
+mpd_type = root.attrib['type']
 if 'minimumUpdatePeriod' in root.attrib:
   mpd_mup = root.attrib['minimumUpdatePeriod']
 else:
@@ -199,6 +250,7 @@ for p in root.findall(nodePeriod):
 
   if 'duration' in p.attrib:
     period_duration = p.attrib['duration']
+    print('duration= ', round(ptTime2Seconds(period_duration)/60), 'min')
   else: 
     period_duration = "NA"
 
@@ -210,5 +262,5 @@ for p in root.findall(nodePeriod):
   print( '  %*s = %*s' % (-30, 'id', -30, period_id))
   print( '  %*s = %*s' % (-30, 'duration', -30, period_duration))
   print( '  %*s = %*s' % (-30, 'start', -30, period_start))
-  doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd)
+  doSegmentTemplate(mpd_ast, mpd_aet, mpd_spd, period_duration)
   
